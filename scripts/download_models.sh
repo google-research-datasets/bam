0644a9e3e7109bd64bb7ae4b3a75be01f8355496
Sherry Yang <sherryy@google.com>
2020-08-29 16:41:25 -0700

    tar.gz model files and script update to untar.

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-mkdir $PWD/models
-
-declare -a arr=(obj)
-
-base_url=https://storage.googleapis.com/benchmark-intp-methods/models/
-
 for name in obj scene scene_only
 do
-    curl -sS $base_url$name.zip > $PWD/models/$name.zip && \
-    unzip $PWD/models/$name.zip -d $PWD/models/ && \
-    rm $PWD/models/$name.zip
+    tar -xvzf $PWD/models/$name.tar.gz -C $PWD/models/
 done
 
 for i in {1..10}
 do
-    curl -sS "{$base_url}scene$i".zip > $PWD/models/scene$i.zip && \
-    unzip $PWD/models/scene$i.zip -d $PWD/models/ && \
-    rm $PWD/models/scene$i.zip
+    tar -xvzf $PWD/models/scene$i.tar.gz -C $PWD/models/
 done
